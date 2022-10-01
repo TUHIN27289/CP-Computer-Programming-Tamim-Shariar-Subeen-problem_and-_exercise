57db8fe1699cdc7e570e3cbad679dd4add4d4e78
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-01 23:26:05 +0600

    Add files via upload

---
+++ Define_odd_or_even_12.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter Number : ";
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<"Even";
+    }
+    else
+    {
+        cout<<"Odd";
+    }
+    getch();
+
+}
