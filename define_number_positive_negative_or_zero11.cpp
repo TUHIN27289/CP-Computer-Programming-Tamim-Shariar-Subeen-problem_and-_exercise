e8ea3801bc274a55b096a3a73b63d655e70a24c8
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-01 23:24:39 +0600

    Add files via upload

---
+++ define_number_positive_negative_or_zero11.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter Number : ";
+    cin>>n;
+    if(n>0)
+    {
+        cout<<"Positive";
+    }
+    else if(n<0)
+    {
+        cout<<"Negative";
+    }
+    else
+    {
+        cout<<"Zero";
+    }
+    getch();
+}
