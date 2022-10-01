4ebd9127fd15f3483444ccc86bc9e2ea6882a25d
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-01 23:17:47 +0600

    Add files via upload

---
+++ Printing_element_sum_4.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter number -1 : ";
+    cin>>a;
+    cout<<"Enter number -2 : ";
+    cin>>b;
+    sum=a+b;
+    cout<<endl<<endl<<a<<" + "<<b<<" 6= "<<sum;
+    getch();
+}
+
