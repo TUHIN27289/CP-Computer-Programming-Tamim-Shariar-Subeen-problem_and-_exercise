8be71671d8af2f6ce5868dc9b2f021590c900579
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-01 23:23:26 +0600

    Add files via upload

---
+++ Define_capital_letter_or_small_letter14.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char ch;
+    cin>>ch;
+    if(ch>='a'&&ch<='z')
+    {
+        cout<<ch<<" Lower Case ";
+    }
+    else
+    {
+        cout<<ch<<" Upper Case";
+    }
+    getch();
+}
