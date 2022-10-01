df1bab924271e2eb815b6284088e25e495c82bd3
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-01 23:22:39 +0600

    Add files via upload

---
+++ Define_capital_letter_or_small_letter13.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"Enter Character : ";
+    cin>>ch;
+    if(ch=='A')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='B')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='C')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='D')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='E')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='F')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='G')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='H')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='I')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='J')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='k')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='L')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='M')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='N')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='O')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='P')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='Q')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='R')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='S')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='T')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='U')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='V')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='W')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='X')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='Y')
+    {
+        cout<<"Upper case ";
+    }
+    else if(ch=='Z')
+    {
+        cout<<"Upper case ";
+    }
+    else
+    {
+        cout<<"Lower case";
+    }
+    getch();
+}
