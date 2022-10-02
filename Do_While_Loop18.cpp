ef9b7fb4cdf4d71a350801848ca8cb63d98f1966
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-02 07:01:11 +0600

    Add files via upload

---
+++ Do_While_Loop18.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    do{
+        cout<<n<<"\t";
+        n++;
+      }
+      while(n<=100);
+
+      getch();
+}
