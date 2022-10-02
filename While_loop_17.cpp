75451371d7cbd8fede628c12e7b950be4126dae7
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-02 07:07:49 +0600

    Add files via upload

---
+++ While_loop_17.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n<=100)
+    {
+        cout<<n<<"\t";
+        n++;
+    }
+    getch();
+}
