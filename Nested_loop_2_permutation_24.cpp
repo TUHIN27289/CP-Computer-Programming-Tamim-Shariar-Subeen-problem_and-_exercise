f9ce8c8b88506dede89c7d8238803cbd7aef12df
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-02 07:04:19 +0600

    Add files via upload

---
+++ Nested_loop_2_permutation_24.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=3;i++)
+    {
+        for(int j=1;j<=3;j++)
+        {
+           if(j!=i)
+           {
+               for(int k=1;k<=3;k++)
+               {
+                   if(k!=i&&k!=j)
+                   {
+                       cout<<i<<"\t"<<j<<"\t"<<k<<"\t"<<endl;
+                   }
+               }
+           }
+        }
+    }
+    getch();
+}
