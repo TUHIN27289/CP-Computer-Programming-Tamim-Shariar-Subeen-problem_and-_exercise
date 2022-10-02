69d5f489bd59b73eb8e70ed5ec08a0849e2626e9
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-02 07:01:41 +0600

    Add files via upload

---
+++ For_loop_16.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter Number to run the loop : ";
+    cin>>n;
+    for(int i=0;i<=n;i++)
+    {
+        cout<<i<<"\t";
+    }
+    getch();
+}
