effc81abab457d478c4c0fab64164a0e40e9ee12
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-02 07:02:01 +0600

    Add files via upload

---
+++ For_loop_021.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+   int n=5;
+   int i=1;
+   for(;;){
+
+    cout<<n<<" X "<<i<<" = "<<n*i<<endl;
+    i=i+1;
+    if(i>10){
+        break;
+    }
+   }
+   getch();
+}
