fbd984d70b1bda493a0d3ccd8be6786fd767cbec
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-02 07:00:50 +0600

    Add files via upload

---
+++ Define_positive_and_negative.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number : ";
+    cin>>n;
+    if(n>=0)
+    {
+        cout<<"Positive";
+    }
+    else
+    {
+        cout<<"Negative";
+    }
+    getch();
+}
