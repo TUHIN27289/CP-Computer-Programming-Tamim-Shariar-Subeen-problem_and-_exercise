436b410a9a91b440c488765e61d2535494c5f416
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-02 07:07:00 +0600

    Add files via upload

---
+++ Use_of_or_operator_and_define_letter_vowel_or_alphabet_15.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char ch;
+    cin>>ch;
+    if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+    {
+        cout<<"Vowel";
+    }
+    else if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    {
+        cout<<"Vowel";
+    }
+    else
+    {
+        cout<<"Constant";
+    }
+    getch();
+}
