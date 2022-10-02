a1b74bfddd1690204a695a538cf507e007aa42b1
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-02 07:05:46 +0600

    Add files via upload

---
+++ Setprecision_partice.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    double n=3.141516171819;
+    double n1=-3.141516171819;
+    cout<<fixed<<setprecision(0)<<n<<"\t\t"<<n1<<endl;
+    cout<<fixed<<setprecision(1)<<n<<"\t\t"<<n1<<endl;
+    cout<<fixed<<setprecision(2)<<n<<"\t\t"<<n1<<endl;
+    cout<<fixed<<setprecision(3)<<n<<"\t\t"<<n1<<endl;
+    cout<<fixed<<setprecision(4)<<n<<"\t\t"<<n1<<endl;
+    cout<<fixed<<setprecision(5)<<n<<"\t\t"<<n1<<endl;
+    cout<<fixed<<setprecision(6)<<n<<"\t"<<n1<<endl;
+    cout<<fixed<<setprecision(7)<<n<<"\t"<<n1<<endl;
+    cout<<fixed<<setprecision(8)<<n<<"\t"<<n1<<endl;
+    cout<<fixed<<setprecision(9)<<n<<"\t"<<n1<<endl;
+    cout<<fixed<<setprecision(10)<<n<<"\t"<<n1<<endl;
+    cout<<fixed<<setprecision(11)<<n<<"\t"<<n1<<endl;
+    cout<<fixed<<setprecision(12)<<n<<"\t"<<n1<<endl;
+    cout<<fixed<<setprecision()<<n<<"\t"<<n1<<endl;
+    getch();
+}
