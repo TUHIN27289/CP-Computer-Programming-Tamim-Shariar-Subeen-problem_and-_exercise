f97cf8023d4fc06384c4fb39ba63f990b77d68c3
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-02 07:05:04 +0600

    Add files via upload

---
+++ Putting_different_range_of_value_and_checking_output.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    n=1;
+    cout<<n<<endl;
+    n=10;
+    cout<<n<<endl;
+    n=100;
+    cout<<n<<endl;
+    n=10000;
+    cout<<n<<endl;
+    n=100000;
+    cout<<n<<endl;
+    n=1000000;
+    cout<<n<<endl;
+    n=10000000;
+    cout<<n<<endl;
+    n=100000000;
+    cout<<n<<endl;
+    n=1000000000;
+    cout<<n<<endl;
+    n=10000000000;
+    cout<<n<<endl;
+    n=100000000000;
+    cout<<n<<endl;
+    n=-1;
+    cout<<n<<endl;
+    n=-10;
+    cout<<n<<endl;
+    n=-100;
+    cout<<n<<endl;
+    n=-10000;
+    cout<<n<<endl;
+    n=-100000;
+    cout<<n<<endl;
+    n=-1000000;
+    cout<<n<<endl;
+    n=-10000000;
+    cout<<n<<endl;
+    n=-100000000;
+    cout<<n<<endl;
+    n=-1000000000;
+    cout<<n<<endl;
+    n=-10000000000;
+    cout<<n<<endl;
+    n=-100000000000;
+    cout<<n<<endl;
+    getch();
+
+}
