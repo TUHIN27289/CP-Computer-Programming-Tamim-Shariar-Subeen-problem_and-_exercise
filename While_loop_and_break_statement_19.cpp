55fc0ae9c3de6a4a2cee12dd3015844c547bf5f6
TUHIN27289 <88925041+TUHIN27289@users.noreply.github.com>
2022-10-02 07:08:08 +0600

    Add files via upload

---
+++ While_loop_and_break_statement_19.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n<=100)
+    {
+        cout<<n<<"\t";
+        n++;
+        if(n==50)
+        {
+            break;
+        }
+    }
+    getch();
+}
